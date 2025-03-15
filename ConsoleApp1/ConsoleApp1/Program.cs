5f6750e066b43140e78753987277e730cc2e737a
TuninRylen <ugurrcancelikk@gmail.com>
2025-03-15 14:13:51 +0300

    v1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿PrintNumbers();
+SumUpToNumber();
+
+void PrintNumbers()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+void SumUpToNumber()
+{
+    Console.Write("Please enter a number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+
+    Console.WriteLine($"The sum of numbers from 1 to {num} is: {sum}");
+}
